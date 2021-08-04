9fabe7883b4df3a5771c3c1268c2e0b7923934f1
Teyzar <thomasandrewzaragoza@gmail.com>
2021-08-04 15:49:05 +0800

    added session for loginuser

--- php/LoginRegistration/login.php
+++ php/LoginRegistration/login.php
@@ -1,26 +1,47 @@
 <?php
     include "$_SERVER[DOCUMENT_ROOT]/php/database/config.php";
-
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-
-    $true = mysqli_query($conn, "SELECT user_email, user_password FROM accounts WHERE user_email = '".$email."' AND  user_password = '".$password."'");
-    $wrongEmail = mysqli_query($conn, "SELECT user_email, user_password FROM accounts WHERE user_email != '".$email."' AND  user_password = '".$password."'");
-    $wrongPass = mysqli_query($conn, "SELECT user_email, user_password FROM accounts WHERE user_email = '".$email."' AND  user_password != '".$password."'");
+    session_start();
 
     if (isset($_POST['submit'])) {
-        if (mysqli_num_rows($true) > 0) {
-            header("Location: successlogin.php");
-        } elseif (!mysqli_num_rows($true)) {
-            header("Location: index.php?error=1");
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        
+        $_SESSION["mail"] = $email;    
+        $_SESSION["pass"] = $password;  
+        $hashpass = md5($password);
+
+        $true = mysqli_query($conn, "SELECT user_email, user_password FROM accounts WHERE user_email = '".$email."' AND  user_password = '".$hashpass."'");
+        $wrongEmail = mysqli_query($conn, "SELECT user_email, user_password FROM accounts WHERE user_email != '".$email."' AND  user_password = '".$hashpass."'");
+        $wrongPass = mysqli_query($conn, "SELECT user_email, user_password FROM accounts WHERE user_email = '".$email."' AND  user_password != '".$hashpass."'");
+
+        $empty = empty($email) || empty($password);
+
+        $hash = md5($email);
+       
+        if (mysqli_num_rows($true)) {
+            sleep(1);
+            $_SESSION['id'] = true;
+            header("Location: /php/Inventory/inventory");
+            
+        } elseif (!mysqli_num_rows($true)) {    
+            header("Location: index?error");
+          
+        } 
+        if ($empty) {
+            header("Location: index?emptyfields=1");
+            
+
         }
         if (mysqli_num_rows($wrongPass)) {
-            header("Location: index.php?errorpassword=1");
+            header("Location: index?errorpassword");
+            
         } elseif (mysqli_num_rows($wrongEmail)) {
-            header("Location: index.php?erroremail=1");
+            header("Location: index?erroremail");
+             
         }
     }
     $conn->close();
+    
     
 ?>
 
